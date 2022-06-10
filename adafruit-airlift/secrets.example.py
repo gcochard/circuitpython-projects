d316c34b4a4a5a40336c5bd53806bd94dae29a51
Greg Cochard <gcochard@users.noreply.github.com>
2022-06-09 21:28:39 -0700

    Create secrets.example.py

---
+++ adafruit-airlift/secrets.example.py
@@ -0,0 +1,7 @@
+secrets = dict(
+    ssid='your-wifi-network',
+    password='your-wifi-password',
+    timezone='America/Los_Angeles',
+    aio_username='your-aio-username',
+    aio_key='your-aio-key',
+)
